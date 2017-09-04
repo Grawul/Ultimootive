7b757ef451595aabd5c0a987f6b0d7b6885b9de2
TheAlcesh <matze1392@hotmail.de>
2017-09-04 23:17:05 +0200

    added basic server classes

---
+++ UltimootiveServer/Classes/UltimootiveServerDatabase/UserModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltimootiveServer.Classes.UltimootiveServerDatabase
+{
+    class UserModel
+    {
+        #region Fields
+        private string username;
+        private string password;
+        #endregion
+
+        #region Constructors
+        #endregion
+
+        #region Properties
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+
+            set
+            {
+                username = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+
+            set
+            {
+                password = value;
+            }
+        }
+        #endregion
+
+        #region methods
+        #endregion
+    }
+}
