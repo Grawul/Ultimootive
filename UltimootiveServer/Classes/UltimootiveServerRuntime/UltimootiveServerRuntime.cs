598cbf8c39cf6cd1cd0aa8b26b9daa07d3da3d48
TheAlcesh <matze1392@hotmail.de>
2017-09-05 23:10:25 +0200

    added basic test connection to mariadb database for the server

--- UltimootiveServer/Classes/UltimootiveServerRuntime/UltimootiveServerRuntime.cs
+++ UltimootiveServer/Classes/UltimootiveServerRuntime/UltimootiveServerRuntime.cs
@@ -8,5 +8,30 @@ namespace UltimootiveServer.Classes
 {
     class UltimootiveServerRuntime
     {
+        #region Fields
+        private UltimootiveServerDatabaseConnection databaseConnection = null;
+        #endregion
+
+        #region Constructors
+        public UltimootiveServerRuntime()
+        {
+            databaseConnection = new UltimootiveServerDatabaseConnection();
+        }
+        #endregion
+
+        #region Methods
+        public void testDatabase()
+        {
+            databaseConnection.testConnection();
+        }
+        public void createDefaultTables()
+        {
+            databaseConnection.createDefaultTables();
+        }
+        public void dropDefaultTables()
+        {
+            databaseConnection.dropDefaultTables();
+        }
+        #endregion
     }
 }
--- UltimootiveServer/Program.cs
+++ UltimootiveServer/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UltimootiveServer.Classes;
 
 namespace UltimootiveServer
 {
@@ -10,6 +11,18 @@ namespace UltimootiveServer
     {
         static void Main(string[] args)
         {
+            UltimootiveServerRuntime run = new UltimootiveServerRuntime();
+
+            // --- Test area
+            // -- Create database stuff
+            // -- GRANT ALL PRIVILEGES ON ultimootivedb.* TO 'db_user'@'%' IDENTIFIED BY PASSWORD 'password' WITH GRANT OPTION;
+            //run.testDatabase();
+            //run.createDefaultTables();
+            //run.dropDefaultTables();
+            // --
+            // ---
+
+            Console.ReadKey();
         }
     }
 }
